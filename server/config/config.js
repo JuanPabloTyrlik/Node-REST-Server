3275ca3431eaa20dbde4d68c31d4edaec2bd3b7a
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-04-21 00:00:49 -0300

    MongoDB Atlas connection, warnings fixed

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,19 @@
 // Port
 
 process.env.PORT = process.env.PORT || 3000;
+
+// Environment
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Databases
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+
+process.env.URLDB = urlDB;
