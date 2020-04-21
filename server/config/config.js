72c43d1b4517ccdf43da7a4d3c8ff9504db69d90
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-04-21 00:17:39 -0300

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://JP:10PHzG3E3bPBbwx2@cluster0-kemco.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
