dba6c2741b5ac43c100ac432d9459c36841782b6
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-04-23 22:46:53 -0300

    Login logic added

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,8 +4,37 @@ const bcrypt = require('bcrypt');
 const Usuario = require('../model/usuario');
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
+    Usuario.findOne({ email: req.body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o contraseña incorrectos'
+                }
+            });
+        }
+
+        if (bcrypt.compareSync(req.body.password, usuarioDB.password)) {
+            return res.json({
+                ok: true,
+                usuarioDB,
+                token: '123'
+            });
+        } else {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o contraseña incorrectos'
+                }
+            });
+        }
     });
 });
 
